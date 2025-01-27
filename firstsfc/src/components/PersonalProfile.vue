474209c4d9c2c53a6ef01f12bc73a3ad45fc3bbb
fslepsot <ftlepsot@student.apc.edu.ph>
2025-01-27 14:35:30 +0800

    updated personalprofile.vue

--- firstsfc/src/components/PersonalProfile.vue
+++ firstsfc/src/components/PersonalProfile.vue
@@ -1,133 +1,169 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>My First Vue Page</title>
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/flexbox-grid.css?version=0.1">
-  <link rel="stylesheet" href="css/pink.css?version=0.1">
-  <link rel="stylesheet" href="css/images.css?version=0.2">
-  <link rel="stylesheet" href="css/shopping.css?version=0.1">
-</head>
-<body>
+<template>
+    <div class="container">
+      <header>
+        <h1>Personal Profile Web Page</h1>
+      </header>
+      <main>
+        <section class="main-content">
+          <div class="about">
+            <h1
+            <p class="ex4">Personal Profile Web page</p></h1>  
+            <a href="https://www.facebook.com/frances.lepsot" target="_blank">Facebook</a>
+            <a href="https://www.instagram.com/_ceslepsot/" target="_blank">Instagram</a>
+            <a href="https://www.linkedin.com/in/frances-sophia-lepsot-121704287/" target="_blank">LinkedIn</a>
+            <h3><p class="ex4">About Me</p></h3>
+            <p>Frances Sophia T. Lepsot is my name, and I am a second-year student at Asia Pacific College.</p>
+            </div>
 
-  <div id="app">
-    <template>
-      <p>{{ message }}</p>
-      <div :class="vueClass">This element is bound to the "pinkBG" class.</div>
-    </template>
-  </div>
-
-  <div id="app2">
-    <template>
-      <div>
-        <figure v-for="x in manyFoods" :key="x.name">
-          <img :src="x.url" alt="Food Image">
-          <figcaption>{{ x.name }}</figcaption>
-        </figure>
-      </div>
-    </template>
-  </div>
-
-  <div id="app3">
-    <template>
-      <form @submit.prevent="addItem">
-        <p>What do you need? <br>
-          <input type="text" required placeholder="Item name.." v-model="itemName">
-        </p>
-        <p>How many? <br>
-          <input type="number" placeholder="Number of items.." v-model="itemNumber">
-        </p>
-        <p>Important?
-          <label>
-            <input type="checkbox" v-model="itemImportant">
-            {{ itemImportant }}
-          </label>
-        </p>
-        <button type="submit">Add item</button>
-      </form>
-      <br>
-      <hr>
-
-      <div>
-        <p><strong>Shopping list:</strong></p>
-        <ul id="ulToFind">
-          <li 
-            v-for="item in shoppingList" 
-            :key="item.name" 
-            :class="{ impClass: item.important }"
-            @click="item.found = !item.found"
-            v-show="!item.found">
-            {{ item.name }}, {{ item.number }}
-          </li>
-        </ul>
-        <ul id="ulFound">
-          <li 
-            v-for="item in shoppingList" 
-            :key="item.name + '-found'" 
-            :class="{ impClass: item.important }"
-            @click="item.found = !item.found"
-            v-show="item.found">
-            {{ item.name }}, {{ item.number }}
-          </li>
-        </ul>
-      </div>
-    </template>
-  </div>
-
-  <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+          <div class="education">
+            <h1>Education and Achievements</h1>
+            <ul>
+            <li>Graduated Elementary at St. Augustine Academy of Pampanga.</li>
+            <li>Graduated Junior High School with Honors from St. Augustine Academy of Pampanga, Class of 2021.</li>
+            <li>Graduated Senior High School with Honors from University of the Assumption, Class of 2023.</li>
+            </ul>
+            </div>
+  
+          <div class="course">
+            <h1>Course</h1>
+            <p>Bachelor of Science in Computer Science with specialization in Cyber Security and Forensics</p> 
+          </div>
+  
+          <div class="hobbies">
+            <h1>Hobbies and Interests</h1>
+            <ul>
+            <li>Travelling</li>
+            <li>Watching movies</li>
+            <li>Listening to music</li>
+            <li>Crocheting</li>
+            </ul>
+            </div>
+        </section>
+  
+        <h1>Vue Example</h1>
+  
+        <div id="app">
+          {{ message }}
+          <div v-bind:class="vueClass">This element is bound to the "pinkBG" class.</div>
+        </div>
+  
+        <div id="app2">
+          <div>
+            <figure v-for="x in manyFoods" :key="x.name">
+              <img :src="x.url" alt="">
+              <figcaption>{{ x.name }}</figcaption>
+            </figure>
+          </div>
+        </div>
+  
+        <div id="app3">
+          <form @submit.prevent="addItem">
+            <p>
+              What do you need? <br />
+              <input type="text" required placeholder="item name.." v-model="itemName" />
+            </p>
+            <p>
+              How many? <br />
+              <input type="number" placeholder="number of items.." v-model="itemNumber" />
+            </p>
+            <p>
+              Important?
+              <label>
+                <input type="checkbox" v-model="itemImportant" />
+                {{ itemImportant }}
+              </label>
+            </p>
+            <button type="submit">Add item</button>
+          </form>
+          <br />
+          <hr />
+  
+          <div>
+            <p><strong>Shopping list:</strong></p>
+            <ul id="ulToFind">
+              <li
+                v-for="item in shoppingList"
+                :key="item.name"
+                :class="{ impClass: item.important }"
+                @click="item.found = !item.found"
+                v-show="!item.found"
+              >
+                {{ item.name }}, {{ item.number }}
+              </li>
+            </ul>
+            <ul id="ulFound">
+              <li
+                v-for="item in shoppingList"
+                :key="item.name + '-found'"
+                :class="{ impClass: item.important }"
+                @click="item.found = !item.found"
+                v-show="item.found"
+              >
+                {{ item.name }}, {{ item.number }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  </template>
+  
   <script>
-    // App 1
-    const app = Vue.createApp({
-      data() {
-        return {
-          message: "Hello from Vue!",
-          vueClass: "pinkBG"
-        };
-      }
-    });
-    app.mount("#app");
-
-    const app2 = Vue.createApp({
-      data() {
-        return {
-          manyFoods: [
-            { name: "Pizza", url: "path/to/pizza.jpg" },
-            { name: "Burger", url: "path/to/burger.jpg" },
-            { name: "Sushi", url: "path/to/sushi.jpg" }
-          ]
-        };
-      }
-    });
-    app2.mount("#app2");
-
-    const app3 = Vue.createApp({
-      data() {
-        return {
-          itemName: "",
-          itemNumber: null,
-          itemImportant: false,
-          shoppingList: []
-        };
+  export default {
+    data() {
+      return {
+        message: "Hello Vue!",
+        vueClass: "pinkBG",
+        manyFoods: [
+          { name: "Pizza", url: "pizza.jpg" },
+          { name: "Burger", url: "burger.jpg" },
+        ],
+        itemName: "",
+        itemNumber: 1,
+        itemImportant: false,
+        shoppingList: [],
+      };
+    },
+    methods: {
+      addItem() {
+        this.shoppingList.push({
+          name: this.itemName,
+          number: this.itemNumber,
+          important: this.itemImportant,
+          found: false,
+        });
+        this.itemName = "";
+        this.itemNumber = 1;
+        this.itemImportant = false;
       },
-      methods: {
-        addItem() {
-          if (this.itemName && this.itemNumber) {
-            this.shoppingList.push({
-              name: this.itemName,
-              number: this.itemNumber,
-              important: this.itemImportant,
-              found: false
-            });
-            this.itemName = "";
-            this.itemNumber = null;
-            this.itemImportant = false;
-          }
-        }
-      }
-    });
-    app3.mount("#app3");
+    },
+  };
   </script>
-</body>
-</html>
+  
+  <style scoped>
+  .container {
+    font-family: Arial, sans-serif;
+    color: #333;
+  }
+  
+  .highlight {
+    color: red;
+  }
+  
+  .pinkBG {
+    background-color: pink;
+  }
+  
+  .impClass {
+    font-weight: bold;
+  }
+  
+  #ulToFind li {
+    cursor: pointer;
+  }
+  
+  #ulFound li {
+    text-decoration: line-through;
+    cursor: pointer;
+  }
+  </style>
