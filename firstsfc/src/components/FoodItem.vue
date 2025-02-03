1b40fd2f8249fb22c7ae5506dde3a4afd567a9ec
fslepsot <ftlepsot@student.apc.edu.ph>
2025-02-03 16:35:26 +0800

    added CommentForm.vue

--- firstsfc/src/components/FoodItem.vue
+++ firstsfc/src/components/FoodItem.vue
@@ -1,27 +1,19 @@
 <template>
-    <div class="apple">
-        <h2>{{ name }}</h2>
-        <p>{{ message }}</p>
+    <div>
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
     </div>
-</template>
-
-<script>
-export default {
+  </template>
+  
+  <script>
+  export default {
     data() {
-        return {
-            name: 'Apples',
-            message: 'I like apples'
-        }
+      return {
+        name: 'Apples',
+        message: 'I like apples and oranges'
+      }
     }
-};
-</script>
-
-<style>
-  .apple {
-        border: dashed black 1px;
-        display: inline-block;
-        margin: 10px;
-        padding: 10px;
-        background-color: lightgreen;
-    }
-</style>              
+  };
+  </script>
+  
+  <style></style>
